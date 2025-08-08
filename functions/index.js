5ccfbace898edfb98fd04654fdad011b8a3ccf4b
Your Name <you@example.com>
2025-08-08 21:01:27 +0900

    변경사항 반영

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,23 @@
-const functions = require('firebase-functions');
-const express   = require('express');
-const cors      = require('cors');
+// functions/index.js
+const express = require('express');
+const cors    = require('cors');
+
+const { onRequest } = require('firebase-functions/v2/https');
+require('dotenv').config();
 
 const createPaymentRouter = require('./routes/createPayment');
-const webhookRouter       = require('./routes/webhook');
+// (웹훅 처리 필요 없으면 아래 줄 지워도 됩니다)
+// const webhookRouter       = require('./routes/payactionWebhook');
 
 const app = express();
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-// 주문 생성 엔드포인트
-app.use('/createPayment', createPaymentRouter);
+// 주문 생성
+app.use('/api', createPaymentRouter);
 
-// 페이액션 입금/매칭 웹훅 엔드포인트
-app.use('/webhook', webhookRouter);
+// 웹훅 엔드포인트 (등록만 해 두시려면 그대로 두세요)
+// app.use('/webhook', webhookRouter);
 
-exports.api = functions.https.onRequest(app);
+// 2nd Gen v2 방식으로 배포
+exports.api = onRequest({ region: 'us-central1' }, app);
